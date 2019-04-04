7882ef98eaf78c6d39381cf2a5cd3697934b42e6
洪宇庄 <yuzhuang@eastmoney.com>
2019-04-04 16:22:02 +0800

    Signed-off-by: 洪宇庄 <yuzhuang@eastmoney.com>

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*- 
+""" 
+Created on 2017-12-08 17:54:20.019635 
+
+@author: 洪宇庄
+"""
+
+from setuptools import setup, find_packages
+setup(
+  	name="pipePiper",
+  	version="2.0",
+  	description="my multiThreading calculation FrameWork",
+  	author="Eric",
+  	author_email="hongyz1993@163.com",
+    packages = find_packages(), 
+)
+
